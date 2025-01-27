74bfa681ae20ccbcda518a25bc36e5434c2c0aeb
mgCepeda <marina.gomez.cepeda@accenture.com>
2025-01-27 10:16:11 +0100

    test

--- ML/intro.py
+++ ML/intro.py
@@ -25,7 +25,7 @@ speed = [99,86,87,88,86,103,87,94,78,77,85,86]
 x = numpy.median(speed)
 print(x)
 
-# Utilice el mode()método SciPy para encontrar el número que aparece con más frecuenciaaa
+# Utilice el mode()método SciPy para encontrar el número que aparece con más frecuencia
 from scipy import stats
 speed = [99,86,87,88,111,86,103,87,94,78,77,85,86]
 x = stats.mode(speed)
